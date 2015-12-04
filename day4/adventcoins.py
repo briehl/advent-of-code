3b10403a385d70b049c14618a33563328004e6c3
Bill Riehl <wjriehl@lbl.gov>
2015-12-04 14:48:13 -0800

    Day 4 part 2... maybe could've been much much worse?

--- day4/adventcoins.py
+++ day4/adventcoins.py
@@ -8,6 +8,14 @@ So if we start with abcde, and putting the number 1095 on the end does this:
 MD5(abcde1095) = 00000blahblahblah,
 
 then the answer is 1095.
+
+Usage:
+python adventcoints.py -f input_file_with_token -n num_prepend_zeros
+
+E.g.
+input_file_with_token contains just the token string to seed the hash
+num_prepend_zeros = number of zeros to look for at the start of the hexified hash
+(5 in the example above)
 """
 
 from __future__ import print_function
@@ -16,20 +24,21 @@ import sys
 import argparse
 import hashlib
 
-def find_coin(token):
+def find_coin(token, num_zeros):
     """
     Don't really see any other way than BRUTE FORCE AWWW YEAH.
     """
     val = 0
+    pre_str = '0'*num_zeros
 
-    while not test_hash(token + str(val)):
+    while not test_hash(token + str(val), pre_str):
         val+=1
 
     return val
 
-def test_hash(s):
+def test_hash(s, pre_str):
     hex_hash = hashlib.md5(s).hexdigest()
-    return hex_hash.startswith('00000')
+    return hex_hash.startswith(pre_str)
 
 def parse_args():
     """
@@ -37,13 +46,14 @@ def parse_args():
     """
     p = argparse.ArgumentParser(description=__doc__.strip())
     p.add_argument("-f", "--input-file", default='', dest="in_file", help="Input file")
+    p.add_argument("-n", "--num-zeros", default=5, type=int, dest="num_zeros", help="Num zeros at the start of the output hash")
     return p.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
     if args.in_file:
         token = open(args.in_file).read().strip()
-        print(find_coin(token))
+        print(find_coin(token, args.num_zeros))
         sys.exit(0)
     else:
         print("No input file!")
