2227bac73d9a04ce3c04d0b90c7fa46695105f4a
Bill Riehl <wjriehl@lbl.gov>
2017-12-04 10:56:49 -0800

    day 2 2017 done!

--- 2017/day2/spreadsheet_checksum.py
+++ 2017/day2/spreadsheet_checksum.py
@@ -18,6 +18,43 @@ def calc_checksum(ss):
         checksum += max(line) - min(line)
     return checksum
 
+
+def get_pairs(arr):
+    i = 0
+    j = 1
+    while i < len(arr):
+        while j < len(arr):
+            yield (arr[i], arr[j])
+            j += 1
+        i += 1
+        j = i + 1
+
+
+def even_division(pair):
+    (a, b) = pair
+    div = a/b
+    if int(div) == div:
+        return int(div)
+    div = b/a
+    if int(div) == div:
+        return int(div)
+    return -1
+
+
+def even_divisor_sum(ss):
+    total = 0
+    for line in ss:
+        for pair in get_pairs(line):
+            val = even_division(pair)
+            if val > 0:
+                total += val
+                break
+    return total
+
+
 if __name__ == "__main__":
     ss = _read_input(INPUT_FILE)
     print(calc_checksum(ss))
+    print(even_divisor_sum(ss))
+    # for pair in get_pairs(['a', 'b', 'c', 'd', 'e']):
+    #     print(pair)
