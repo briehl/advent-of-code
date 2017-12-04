b1dda9ff18f27c0291e8dac4abd9cb5913526236
Bill Riehl <wjriehl@lbl.gov>
2017-12-04 10:28:21 -0800

    2017 day 1 part 2

--- 2017/day1/sequence_sum.py
+++ 2017/day1/sequence_sum.py
@@ -8,28 +8,30 @@ def _read_input(in_file):
     return sequence
 
 
-def get_next(idx, seq):
+def get_next(idx, n, seq):
     """
-    Peeks ahead one value forward from idx.
-    if idx+1 > len(seq), wrap around.
+    Peeks ahead n steps forward from idx. n should be > 1.
+    if idx+n > len(seq), wrap around.
 
     Returns as an int
     """
-    next_idx = idx+1
+    next_idx = idx+n
     if next_idx >= len(seq):
         next_idx = next_idx - len(seq)
     return int(seq[next_idx])
 
 
-def calc_seq_sum(sequence):
+def calc_step_seq_sum(sequence, step_size):
     total = 0
     for i in range(len(sequence)):
         cur_val = int(sequence[i])
-        next_val = get_next(i, sequence)
+        next_val = get_next(i, step_size, sequence)
         if cur_val == next_val:
             total += cur_val
     return total
 
 
 if __name__ == "__main__":
-    print(calc_seq_sum(_read_input(INPUT_FILE)))
+    sequence = _read_input(INPUT_FILE)
+    print(calc_step_seq_sum(sequence, 1))
+    print(calc_step_seq_sum(sequence, int(len(sequence)/2)))
